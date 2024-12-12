56a2beebc901252f9c0d540012d1ef2919f1101e
Bjarte Myksvoll <bjarte.myksvoll@live.no>
2024-12-12 07:00:13 +0100

    Moved n m logic to SmoothSpline

--- src/ForwardCurve.jl
+++ src/ForwardCurve.jl
@@ -1,67 +1,73 @@
 # Module for maximum smoothness forward curve
-module ForwardCurve
-
 using Dates
 using Plots
 using LinearAlgebra
 using Polynomials
 
-include("SmoothSpline.jl")
+include("smoothspline.jl")
 
-const DAYS_PER_YEAR = 365.25
+const DAYS_PER_YEAR = 365
 
-#= struct ForwardCurve
-    unit::String
-    currency::String
-    trade_date::DateTime
-    instruments::Dict
-    taus::Vector{Tuple{Int,Int}}
-    knots::Vector{Int}
-    prices::Vector{Float64}
-    spline::SmoothSpline
+struct ForwardCurve
+	unit::String
+	currency::String
+	trade_date::DateTime
+	instruments::DataFrame
+	spline::SmoothSpline
 
-    function ForwardCurve(unit::String, currency::String, trade_date::DateTime, instruments::Dict)
-        instruments["start"] = [Dates.days(start_date - trade_date) for start_date in instruments["start_date"]]
-        instruments["end"] = [Dates.days(end_date - trade_date) for end_date in instruments["end_date"]]
+	function ForwardCurve(unit::String, currency::String, trade_date::DateTime, instruments::DataFrame)
+		# Calculate the number of days from trade_date to start_date and end_date
+		instruments.start_day = Dates.days.(instruments.start_date - trade_date)
+		instruments.end_day = Dates.days.(instruments.end_date - trade_date)
+		instruments.start_time = instruments.start_day / DAYS_PER_YEAR
+		instruments.end_time = instruments.end_day / DAYS_PER_YEAR
 
-        taus = collect(zip(instruments["start"], instruments["end"]))
-        knots = [0; sort(unique(vcat(instruments["start"], instruments["end"])))]
-        prices = instruments["price"]
-        spline = SmoothSpline(taus, knots, prices)
+		# Create a vector of tuples representing the start and end days
+		#taus = collect(zip(instruments.start_day, instruments.end_day))
 
-        new(unit, currency, trade_date, instruments, taus, knots, prices, spline)
-    end
+		# Create a sorted vector of unique days from both start_day and end_day
+		#knots = [0; sort(unique(vcat(instruments.start_day, instruments.end_day)))]
+
+		spline = SmoothSpline(instruments.start_day, instruments.end_day, instruments.price)
+		new(unit, currency, trade_date, instruments, spline)
+	end
 end
- =#
+
+
+function plot_curve(forward_curve::ForwardCurve)
+	plot_spline(forward_curve.spline)
+end
+
+#= 
 # Define a function to calculate the difference in years
 difference_in_years(start_date::DateTime, end_date::DateTime) = (end_date - start_date).value / (DAYS_PER_YEAR * 24 * 60 * 60 * 1000)
 
 function (fc::ForwardCurve)(times::Vector{Int})
-    return fc.spline.calculate_polynomial_value(times)
+	return fc.spline.calculate_polynomial_value(times)
 end
 
 function (fc::ForwardCurve)(start_time::Int, end_time::Int)
-    return fc.spline.calculate_spline_average(start_time, end_time)
+	return fc.spline.calculate_spline_average(start_time, end_time)
 end
 
-#= 
+
 function get_price_intervals(fc::ForwardCurve, interval::Int=7)
-    start_days = 0:interval:fc.knots[end]-1
-    end_days = min.(start_days .+ interval, fc.knots[end])
+	start_days = 0:interval:fc.knots[end]-1
+	end_days = min.(start_days .+ interval, fc.knots[end])
 
-    prices = [fc(start_days, end_days) for (start_days, end_days) in zip(start_days, end_days)]
+	prices = [fc(start_days, end_days) for (start_days, end_days) in zip(start_days, end_days)]
 
-    return hcat(start_days, end_days, prices)
+	return hcat(start_days, end_days, prices)
 end
 
 function plot(fc::ForwardCurve)
-    #plot(title="Maximum smoothness forward curve", xlabel="Time", ylabel="Price", legend=false, size=(800, 400))
-    plot_spline(fc.spline)
+	#plot(title="Maximum smoothness forward curve", xlabel="Time", ylabel="Price", legend=false, size=(800, 400))
+	plot_spline(fc.spline)
 
-    for (start_time, end_time, price) in zip(fc.instruments["start"], fc.instruments["end"], fc.instruments["price"])
-        plot!([start_time, end_time], [price, price], color="darkgrey", label="")
-    end
-    display(plot!())
+	for (start_time, end_time, price) in zip(fc.instruments["start"], fc.instruments["end"], fc.instruments["price"])
+		plot!([start_time, end_time], [price, price], color="darkgrey", label="")
+	end
+	display(plot!())
 end
+
  =#
-end
--- src/SmoothSpline.jl
+++ src/SmoothSpline.jl
@@ -1,184 +1,197 @@
 # Module for maximum smoothness forward curve
-module SmoothSpline
-
 using Dates
 using Plots
 using LinearAlgebra
 using Polynomials
 using BlockDiagonals
 
-#= 
+
 struct SmoothSpline
-    taus::Vector{Tuple{Int,Int}}
-    knots::Vector{Int}
-    prices::Vector{Float64}
-    n::Int
-    m::Int
-    polynomial_intervals::Vector{Vector{Int}}
-    H::Matrix{Float64}
-    A::Matrix{Float64}
-    B::Vector{Float64}
-    X::Vector{Vector{Float64}}
-    # Inner constructor initializes only a subset of the fields
-    function SmoothSpline(taus::Vector{Tuple{Int,Int}}, knots::Vector{Int}, prices::Vector{Float64})
-        n = length(knots) - 1
-        m = length(taus)
-        polynomial_intervals = [[start, knots[i+1]] for (i, start) in enumerate(knots[1:end-1])]
-        H = calc_big_H(knots, n)
-        A = calc_big_A(taus, knots, m, n)
-        B = calc_B(taus, prices, m, n)
-        X = solve_lineq(H, A, B)
-        new(taus, knots, prices, n, m, polynomial_intervals, Matrix{Float64}(undef, 0, 0), Matrix{Float64}(undef, 0, 0), Float64[], Vector{Vector{Float64}}(undef, 0))
-    end
+	taus::Vector{Tuple{Int, Int}}
+	knots::Vector{Int}
+	prices::Vector{Float64}
+	n::Int
+	m::Int
+	polynomials::Vector{Polynomial}
+	H::Matrix{Float64}
+	A::Matrix{Float64}
+	B::Vector{Float64}
+	X::Matrix{Float64}
+
+	# Inner constructor initializes only a subset of the fields
+	function SmoothSpline(tau_begin::Vector{Int}, tau_end::Vector{Int}, prices::Vector{Float64})
+
+		# Create a vector of tuples representing the start and end days
+		taus = collect(zip(tau_begin, tau_end))
+
+		# Create a sorted vector of unique days from both start_day and end_day
+		knots = [0; sort(unique(vcat(tau_begin, tau_end)))]
+
+		n = length(knots) - 1
+		m = length(taus)
+
+		# Set up problem
+		H = calc_big_H(knots, n)
+		A = calc_big_A(taus, knots, m, n)
+		B = calc_B(taus, prices, m, n)
+
+		# Solve problem
+		X = solve_lineq(n, H, A, B)
+		# Set up polynomials from solution X
+		polys = [Polynomial(reverse(coeffs), :t) for coeffs in eachrow(X)]
+		new(taus, knots, prices, n, m, polys, H, A, B, X)
+	end
 end
- =#
 
-# Polynomial function
-polynomial_function(t::Int, coef::Vector{Float64}) = polyval(Polynomial(coef), t)
 
-# Polynomial integral
-polynomial_integral(s::Int, coef::Vector{Float64}) = polyint(Polynomial(coef))(s)
-
-#= # Calculate polynomial point values
+#= 
+# Calculate polynomial point values
 function calculate_polynomial_value(spline::SmoothSpline, times::Vector{Int})
-    prices = zeros(Float64, length(times))
-    for (i, interval) in enumerate(polynomial_intervals)
-        start_time, end_time = interval
-        mask = (times .>= start_time) .& (times .<= end_time)
-        prices[mask] .= [polynomial_function(time, X[i]) for time in times[mask]]
-    end
-    return prices
-end
+	prices = zeros(Float64, length(times))
+	for (i, interval) in enumerate(polynomial_intervals)
+		start_time, end_time = interval
+		mask = (times .>= start_time) .& (times .<= end_time)
+		prices[mask] .= [polynomial_function(time, X[i]) for time in times[mask]]
+	end
+	return prices
+end =#
 
 # Calculate spline average
-function calculate_spline_average(spline::SmoothSpline, start_time::Int, end_time::Int)
-    if start_time > end_time
-        error("Start is after end")
-    end
+function average_price(spline::SmoothSpline, start_day::Int, end_day::Int)
+	if start_day > end_day
+		error("Start is after end")
+	end
 
-    if start_time < knots[1] || end_time > knots[end]
-        error("Integration bounds are outside the range of the ")
-    end
+	if start_day < spline.knots[1] || end_day > spline.knots[end]
+		error("Integration bounds are outside the range of the spline")
+	end
 
-    total_integral = 0.0
-    total_duration = end_time - start
-    for (i, coefficients) in enumerate(X)
-        segment_start = max(start, knots[i])
-        segment_end = min(end_time, knots[i+1])
+	total_integral = 0.0
+	total_duration = end_day - start_day
+	for (i, poly) in enumerate(spline.polynomials)
+		segment_start = max(start_day, spline.knots[i])
+		segment_end = min(end_day, spline.knots[i+1])
 
-        if segment_start < segment_end
-            segment_integral = polynomial_integral(segment_end, coefficients) - polynomial_integral(segment_start, coefficients)
-            total_integral += segment_integral
-        end
-    end
+		if segment_start < segment_end
+			segment_integral = integrate(poly, segment_start, segment_end)
+			total_integral += segment_integral
+		end
+	end
 
-    return total_integral / total_duration
+	return total_integral / total_duration
 end
- =#
+
+
 # Calculate H matrix
 function calc_H(tau_b::Int, tau_e::Int)
-    return [
-        (144/5)*(tau_e^5-tau_b^5) 18*(tau_e^4-tau_b^4) 8*(tau_e^3-tau_b^3) 0 0;
-        18*(tau_e^4-tau_b^4) 12*(tau_e^3-tau_b^3) 6*(tau_e^2-tau_b^2) 0 0;
-        8*(tau_e^3-tau_b^3) 6*(tau_e^2-tau_b^2) 4*(tau_e-tau_b) 0 0;
-        0 0 0 0 0;
-        0 0 0 0 0
-    ]
+	return [
+		(144/5)*(tau_e^5-tau_b^5) 18*(tau_e^4-tau_b^4) 8*(tau_e^3-tau_b^3) 0 0;
+		18*(tau_e^4-tau_b^4) 12*(tau_e^3-tau_b^3) 6*(tau_e^2-tau_b^2) 0 0;
+		8*(tau_e^3-tau_b^3) 6*(tau_e^2-tau_b^2) 4*(tau_e-tau_b) 0 0;
+		0 0 0 0 0;
+		0 0 0 0 0
+	]
 end
 
 # Calculate big H matrix
 function calc_big_H(knots, n)
-    h_matrices = [calc_H(knots[i], knots[i+1]) for i in 1:n]
-    return BlockDiagonal(h_matrices)
+	h_matrices = [calc_H(knots[i], knots[i+1]) for i in 1:n]
+	return BlockDiagonal(h_matrices)
 end
 
 # Calculate integral constraint
 function calc_integral_constraint(tau_b::Int, tau_e::Int)
-    return [
-        (tau_e^5 - tau_b^5) / 5
-        (tau_e^4 - tau_b^4) / 4
-        (tau_e^3 - tau_b^3) / 3
-        (tau_e^2 - tau_b^2) / 2
-        tau_e - tau_b
-    ]
+	return [
+		(tau_e^5 - tau_b^5) / 5
+		(tau_e^4 - tau_b^4) / 4
+		(tau_e^3 - tau_b^3) / 3
+		(tau_e^2 - tau_b^2) / 2
+		tau_e - tau_b
+	]
 end
 
 # Calculate knot constraints
 function calc_knot_constraints(u_j::Int)
-    return [
-        u_j^4 u_j^3 u_j^2 u_j 1;
-        4*u_j^3 3*u_j^2 2*u_j 1 0;
-        12*u_j^2 6*u_j 2 0 0
-    ]
+	return [
+		u_j^4 u_j^3 u_j^2 u_j 1;
+		4*u_j^3 3*u_j^2 2*u_j 1 0;
+		12*u_j^2 6*u_j 2 0 0
+	]
 end
 
 # Calculate B matrix
 function calc_B(taus, prices, m, n)
-    B = zeros(Float64, 3 * n + m - 2)
-    for (i, (tau_b, tau_e)) in enumerate(taus)
-        B[3*(n-1)+1+i] = prices[i] * (tau_e - tau_b)
-    end
-    return B
+	B = zeros(Float64, 3 * n + m - 2)
+	for (i, (tau_b, tau_e)) in enumerate(taus)
+		B[3*(n-1)+1+i] = prices[i] * (tau_e - tau_b)
+	end
+	return B
 end
 
 # Solve linear equation
 function solve_lineq(n, H, A, B)
-    top = hcat(2 * H, transpose(A))
-    btm = hcat(A, zeros(size(A, 1), size(transpose(A), 2)))
-    A_merged = vcat(top, btm)
-    B_merged = vcat(zeros(size(top, 2) - length(B)), B)
-    Solution = A_merged \ B_merged
-    X = transpose(reshape(Solution[1:n*5], 5, n))
-    return Matrix(X)
+	top = hcat(2 * H, transpose(A))
+	btm = hcat(A, zeros(size(A, 1), size(transpose(A), 2)))
+	A_merged = vcat(top, btm)
+	B_merged = vcat(zeros(size(top, 2) - length(B)), B)
+	Solution = A_merged \ B_merged
+	X = transpose(reshape(Solution[1:n*5], 5, n))
+	return Matrix(X)
 
 end
 
 # Calculate big A matrix
 function calc_big_A(taus, knots, m, n)
-    A = zeros(Float64, 3 * n + m - 2, 5 * n)
+	A = zeros(Float64, 3 * n + m - 2, 5 * n)
 
-    for (i, knot) in enumerate(knots[2:end-1])
-        c1 = calc_knot_constraints(knot)
-        A[(3*i-2):(3*i), (5*i-4):(5*i)] = c1
-        A[(3*i-2):(3*i), (5*i+1):(5*i+5)] = -c1
-    end
+	for (i, knot) in enumerate(knots[2:end-1])
+		c1 = calc_knot_constraints(knot)
+		A[(3*i-2):(3*i), (5*i-4):(5*i)] = c1
+		A[(3*i-2):(3*i), (5*i+1):(5*i+5)] = -c1
+	end
 
-    # End constraint last knot:first derivative in knot = 0
-    A[3*(n-1)+1, end-4:end] = [4 * knots[end]^3, 3 * knots[end]^2, 2 * knots[end], 1, 0]
+	# End constraint last knot:first derivative in knot = 0
+	A[3*(n-1)+1, end-4:end] = [4 * knots[end]^3, 3 * knots[end]^2, 2 * knots[end], 1, 0]
 
-    # No arbitrage constraints
-    for (i, tau) in enumerate(taus)
-        tau_b, tau_e = tau
-        for j in 2:length(knots)
-            if tau_e > knots[j-1] && tau_b < knots[j]
-                t1 = max(tau_b, knots[j-1])
-                t2 = min(knots[j], tau_e)
-                A[3*(n-1)+1+i, (5*(j-1)-4):(5*(j-1))] = calc_integral_constraint(t1, t2)
-            end
-        end
-    end
+	# No arbitrage constraints
+	for (i, tau) in enumerate(taus)
+		tau_b, tau_e = tau
+		for (j, knot) in enumerate(knots[2:end])
+			prev_knot = knots[j]  # j in enumerate starts from 1, so knots[j] is actually the previous knot
+			if tau_e > prev_knot && tau_b < knot
+				t1 = max(tau_b, prev_knot)
+				t2 = min(knot, tau_e)
+				A[3*(n-1)+1+i, (5*j-4):(5*j)] = calc_integral_constraint(t1, t2)
+			end
+		end
+	end
 
-    return A
+	return A
 end
 
+function plot_spline(spline::SmoothSpline, resolution::Int = 1000)
+	plt = plot(size = (1200, 800))  # Initialize an empty plot
+	for (i, coeffs) in enumerate(eachrow(spline.X))
+		p = Polynomial(reverse(coeffs))
+		interval_start = spline.knots[i]
+		interval_end = spline.knots[i+1]
 
-function plot_spline(X::Matrix{Float64}, knots::Vector{Int}, resolution::Int=1000)
-    plt = plot()  # Initialize an empty plot
-    for (i, coeffs) in enumerate(eachrow(X))
-        p = Polynomial(reverse(coeffs))
-        interval_start = knots[i]
-        interval_end = knots[i+1]
+		# Generate points to plot the polynomial
+		xs = range(interval_start, stop = interval_end, length = 100)
+		ys = p.(xs)
 
-        # Generate points to plot the polynomial
-        xs = range(interval_start, stop=interval_end, length=100)
-        ys = p.(xs)
+		# Plot the polynomial over the interval
+		plot!(xs, ys)
+	end
 
-        # Plot the polynomial over the interval
-        plot!(xs, ys)
-    end
-    # Display the plot
-    display(plt)
+	# Iterate over the start_date, end_date, and price using zip and enumerate
+	for (i, (start_day, end_day, price)) in enumerate(zip(instruments.start_day, instruments.end_day, instruments.price))
+		name = instruments.name[i]
+		# Plot a horizontal line for the price of the instrument
+		plot!(plt, [start_day, end_day], [price, price], label = name, lw = 2)
+	end
 
-end
+	# Display the plot
+	display(plt)
 
 end
